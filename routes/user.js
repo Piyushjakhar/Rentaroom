f86d083ebcf729a2269d5f7f28dde97bee8c2b5c
Piyush Jakhar <piyushjakhar@Piyushs-MacBook-Pro.local>
2021-12-19 20:50:27 +0530

    integrated validations

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const {registerUser, getAllUsers, getUserDetails, deleteUser, loginUser} = require('../controller/userController');
 const {userRegisterMiddleware, userDeletionMiddleware, loginUserMiddleware} = require("../middlewares/userMiddlewares");
 const {verifyToken} = require("../middlewares/auth");
+const {loginValidation, registerValidation} = require('../validations/userValidations');
 
 
 // user routes
-router.post("/register", userRegisterMiddleware, registerUser);
+router.post("/register", registerValidation, userRegisterMiddleware, registerUser);
 router.get('/', getAllUsers);
 router.get('/details', verifyToken, getUserDetails);
-router.delete('/', userDeletionMiddleware, deleteUser);
-router.get('/login', loginUserMiddleware, loginUser)
+router.delete('/', verifyToken, userDeletionMiddleware, deleteUser);
+router.get('/login', loginValidation, loginUserMiddleware, loginUser)
 
 
 module.exports = function (app) {
---
+++ validations/userValidations.js
@@ -0,0 +1,41 @@
+const { validate, ValidationError, Joi } = require('express-validation')
+const genFunctions = require('../utility/genFunctions')
+const status_code = require('../utility/statusCodes');
+
+const loginValidation = (req, res, next) => {
+    const body = req.body;
+
+        const schema = Joi.object({
+            email: Joi.string().email().required(),
+            password: Joi.min(6).max(12).required()
+        })
+        const {error, value} = schema.validate(body);
+        if (error == null) {
+            return next();
+        }
+        else {
+            genFunctions.sendResponse(error.details[0].message, status_code.HTTP_404_BAD_REQUEST, req, res, null);
+        }
+        
+    
+}
+
+const registerValidation = (req, res, next) => {
+        const body = req.body;
+        const schema =  Joi.object({
+            name: Joi.string().required(),
+            email: Joi.string().email().required(),
+            phone: Joi.string().length(10).pattern(/^[0-9]+$/).required(),
+            password: Joi.string().min(6).max(12).required()
+        })
+        const {error, value} = schema.validate(body);
+        if (error == null) {
+            return next();
+        }
+        else {
+            // res.json(error);
+            genFunctions.sendResponse(error.details[0].message, status_code.HTTP_404_BAD_REQUEST, req, res, null);
+        }
+}
+
+module.exports = {loginValidation, registerValidation}
