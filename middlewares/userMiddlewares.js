f8535666cec835d87623a9d2217ac7aef200799c
Piyush Jakhar <piyushjakhar@Piyushs-MacBook-Pro.local>
2021-12-19 17:01:54 +0530

    fixed db bug

--- middlewares/userMiddlewares.js
+++ middlewares/userMiddlewares.js
@@ -4,7 +4,7 @@ const status_code = require('../utility/statusCodes');
 
 const userRegisterMiddleware = (req, res, next) => {
     const phone = req.body.phone;
-    let tempsql = "SELECT count(*) as no_of_users from user where phone=? AND deleted_at IS NULL"; 
+    let tempsql = "SELECT count(*) as no_of_users from User where phone=? AND deleted_at IS NULL"; 
     connection.query(tempsql, [phone], (err, rows) => {
         if (err) {
             genFunctions.sendResponse(err, status_code.HTTP_404_BAD_REQUEST, req, res, null);
@@ -23,7 +23,7 @@ const userRegisterMiddleware = (req, res, next) => {
 
 const userDeletionMiddleware = (req, res, next) => {
     const id = req.body.id;
-    let tempsql = "SELECT count(*) as no_of_users from user where id=? AND deleted_at IS NULL"; 
+    let tempsql = "SELECT count(*) as no_of_users from User where id=? AND deleted_at IS NULL"; 
     connection.query(tempsql, [id], (err, rows) => {
         if (err) {
             genFunctions.sendResponse(err, status_code.HTTP_404_BAD_REQUEST, req, res, null);
@@ -42,7 +42,7 @@ const userDeletionMiddleware = (req, res, next) => {
 
 const loginUserMiddleware = (req, res, next) => {
     const phone = req.body.phone;
-    let tempsql = "SELECT count(*) as no_of_users from user where phone=? AND deleted_at IS NULL";
+    let tempsql = "SELECT count(*) as no_of_users from User where phone=? AND deleted_at IS NULL";
     connection.query(tempsql, [phone], (err, rows) => {
         if (err) {
             genFunctions.sendResponse(err, status_code.HTTP_404_BAD_REQUEST, req, res, null);
