c2d8dbe3889cdc1435f5fba9d73b6207c982e128
piyushjakhar <piyushjakhar08@gmail.com>
2022-01-25 12:13:09 +0530

    added security

--- keys.js
+++
@@ -1,3 +0,0 @@
-const SECRET_KEY = "PIYUSHJAKHAR"
-
-module.exports = {SECRET_KEY}
--- utility/authentication.js
+++ utility/authentication.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const {SECRET_KEY} = require("../keys");
+const {SECRET_KEY} = require("../utility/keys");
 const expiry_time = 24 * 60 * 60 // 1 day
 
 const generateToken = (params) => {
